89a16fb4ea8bf000746b00190acd22196896862f
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-19 02:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,5 +30,13 @@ function assumesBase10 (n) {
   return parseInt(n, 10)
 }
 
+function preserveDecimal (n) {
+  return parseFloat(2.222)
+}
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
 
 
