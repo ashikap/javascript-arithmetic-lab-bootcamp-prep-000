e2ab617e8f1fd755b30c3a6eeee127285505ca73
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-19 02:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ function inc (a) {
   return (a + 1)
 }
 
+function returnsNaNAsAppropriate ()
+
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+
 function dec (a) { 
   return (a - 1)
 }
