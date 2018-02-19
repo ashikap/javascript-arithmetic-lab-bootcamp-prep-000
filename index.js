e67b48160a722a845b7e47178c22c7a28bac57eb
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-19 02:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,21 +34,3 @@ function returnsNaNAsAppropriate (n) {
   return (true)
 }
 
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-
-
