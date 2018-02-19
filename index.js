85b8f390b4f21fb9033dcfdca6dc7691385bb0c6
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-19 02:19:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,9 +34,5 @@ function preserveDecimal (n) {
   return parseFloat(2.222)
 }
 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
 
 
